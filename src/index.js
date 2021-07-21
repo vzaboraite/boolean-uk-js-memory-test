0266129492a6ace905605ba58224104f20cd3b89
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-21 16:00:30 +0100

    Add instructions and TODOs.

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/*
+- Show the user 4 random numbers between 1 and 100 using an alert
+- Ask them to enter the numbers in the right order using a prompt
+    - Delay the prompt by 15 seconds (so they have time to forget)
+- Check how many numbers they entered correctly
+- Show the user their result
+*/
+
+/*
+TODOs:
+1.1 generate random number between two values(use Math.random())
+1.2 create an array of four random numbers between 1 and 100
+1.3 alert generated numbers()
+
+2.1 use prompt for the question(ask user to enter four numbers separated by spaces)
+2.2 save user input to a variable
+2.3 parse entered input to an array of four numbers
+2.4 delay prompt(use setTimeout())
+
+3. count correct numbers(if they were entered in correct sequence)
+
+4. alert user test result
+*/
+
