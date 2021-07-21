8be27eb8e6149ef0d5a43b06f7fd02beaae48f37
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-21 17:05:48 +0100

    Complete TODO parts between 1.1 and 1.3.

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@
 
 /*
 TODOs:
-1.1 generate random number between two values(use Math.random())
-1.2 create an array of four random numbers between 1 and 100
-1.3 alert generated numbers()
+```1.1 generate random number between two values(use Math.random())
+```1.2 create an array of four random numbers between 1 and 100
+```1.3 alert generated numbers()
 
 2.1 use prompt for the question(ask user to enter four numbers separated by spaces)
 2.2 save user input to a variable
@@ -22,3 +22,40 @@ TODOs:
 4. alert user test result
 */
 
+/* function generates random number between two values, including passed-in values */
+function generateRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  let result = Math.floor(Math.random() * (max - min + 1) + min);
+  return result;
+}
+
+// const generatedNumber = generateRandomNumber(1, 100);
+// console.log("generatedNumber: ", generatedNumber);
+
+/*  - use generateRandomNumber(), 
+    - return an array of 4 generated random numbers between 1 and 100 */
+function buildNumberArray() {
+  const generatedNumbers = [];
+
+  for (let i = 0; i < 4; i++) {
+    generatedNumbers.push(generateRandomNumber(1, 100));
+  }
+  return generatedNumbers;
+}
+
+// console.log(buildNumberArray());
+
+/* create an alert message using buildNumberArray() */
+function createAlertMessage() {
+  let message = `***MEMORY TEST***\n\nMemorize these numbers and their order\n\n`;
+  let stringOfNumbers = buildNumberArray().join(`           `);
+
+  message += `${stringOfNumbers}\n\nWhen you are ready, press OK or Enter`;
+
+  return message;
+}
+
+// console.log(createAlertMessage());
+
+alert(createAlertMessage());
